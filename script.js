7cf8affa5232d6f2f0c555e1e1c083c14f559301
Jakub Wasilewski <jakwasi01@gmail.com>
2023-10-13 00:22:57 +0200

    offcanvas pattern made

--- script.js
+++ script.js
@@ -3,32 +3,39 @@ let heartIcon = document.querySelectorAll(".fa-heart");
 let crl = document.getElementsByClassName("carouselItems");
 
 
+// heartIcon.forEach(function(event){
+//   event.addEventListener("click",function(){
+//       event.preventDefault();
+//       event.stopPropagation();
+//   });
+// });
 
-preview.forEach(function(myTile){
-  myTile.addEventListener("click",function(event){
-    if(event.target.closest(".fa-heart")){}else{
-    let previewScreen = document.createElement("div");
-    previewScreen.setAttribute("class", "container-lg previewScreen d-flex flex-row-reverse bd-highlight shadow-sm");
-    document.body.appendChild(previewScreen);
-    document.body.style.overflow = "hidden";
+
+// preview.forEach(function(myTile){
+//   myTile.addEventListener("click",function(event){
+//     if(event.target.closest(".fa-heart")){}else{
+//     let previewScreen = document.createElement("div");
+//     previewScreen.setAttribute("class", "container-lg previewScreen d-flex flex-row-reverse bd-highlight shadow-sm");
+//     document.body.appendChild(previewScreen);
+//     document.body.style.overflow = "hidden";
     
 
-    const btnClose = document.createElement("button");
-    btnClose.setAttribute("class","btn-close p-3 bd-highlight");
-    btnClose.type="button";
-    btnClose.ariaLabel="Close";
-    previewScreen.appendChild(btnClose);
+//     const btnClose = document.createElement("button");
+//     btnClose.setAttribute("class","btn-close p-3 bd-highlight");
+//     btnClose.type="button";
+//     btnClose.ariaLabel="Close";
+//     previewScreen.appendChild(btnClose);
 
-    btnClose.addEventListener("click",function(){
-        previewScreen.remove();
-        document.body.style.overflow = "auto";
+//     btnClose.addEventListener("click",function(){
+//         previewScreen.remove();
+//         document.body.style.overflow = "auto";
 
-    });
-  }
-  });
-});
+//     });
+//   }
+//   });
+// });
 
-let isLiked = false;
+
 
 
 
@@ -43,14 +50,14 @@ heartIcon.forEach(function(icon) {
       icon.classList.add("fa-solid");
       icon.classList.add("fa-heart");
       icon.classList.remove("unactive");
-      icon.classList.add("actives");
+      icon.classList.add("active");
       icon.style.color = "#e92907";
       icon.addEventListener("animationend", function() {
         icon.classList.remove("beat-animation");
       });
     } 
     
-   else if(icon.classList.contains("actives")){
+   else if(icon.classList.contains("active")){
             icon.classList.add("unactive");
             icon.classList.remove("fa-sharp");
             icon.classList.remove("fa-solid");
@@ -67,15 +74,6 @@ heartIcon.forEach(function(icon) {
   });
   
 });
-
-
-
-// data-bs-container="body" 
-// data-bs-toggle="popover" 
-// data-bs-placement="top" 
-// data-bs-content="Dodano do ulubionych"
-
-
 
 
 
