ca08cbd71b6dcb7f9613c15a033c593b6176c24c
Jakub Wasilewski <jakwasi01@gmail.com>
2023-12-26 16:49:08 +0100

    small change

--- public/script.js
+++ public/script.js
@@ -15,14 +15,14 @@ const a = document.getElementById("anim");
 let i = 0;
 
 document.addEventListener("DOMContentLoaded", function () {
-  const likedTiles = JSON.parse(localStorage.getItem('likedTiles')) || [];
+  const likedTiles = JSON.parse(localStorage.getItem("likedTiles")) || [];
 
   const heartIcon = document.querySelectorAll(".fa-heart");
 
   heartIcon.forEach(function (icon) {
     icon.addEventListener("click", function () {
       const parentTile = findParentTile(icon);
-       
+
       if (icon.classList.contains("unactive")) {
         icon.classList.add("beat-animation");
         icon.classList.remove("fa-regular");
@@ -33,10 +33,8 @@ document.addEventListener("DOMContentLoaded", function () {
         icon.classList.remove("unactive");
         icon.classList.add("active");
         icon.style.color = "#e92907";
-        
-        
-        
-      //sprawdzic czy rodzic posiada element z aktywnym sercem
+
+        //sprawdzic czy rodzic posiada element z aktywnym sercem
 
         icon.addEventListener("animationend", function () {
           icon.classList.remove("beat-animation");
@@ -47,8 +45,11 @@ document.addEventListener("DOMContentLoaded", function () {
         addToLiked(icon);
         if (parentTile) {
           parentTile.classList.add("active");
-          }
-        console.log("ID ikony po dodaniu do polubionych:", icon.getAttribute("data-unique-id"));
+        }
+        console.log(
+          "ID ikony po dodaniu do polubionych:",
+          icon.getAttribute("data-unique-id")
+        );
       } else if (icon.classList.contains("active")) {
         icon.classList.add("unactive");
         icon.classList.remove("fa-sharp");
@@ -59,7 +60,7 @@ document.addEventListener("DOMContentLoaded", function () {
         icon.classList.remove("active");
         icon.style.color = "rgb(30 48 80)";
         icon.classList.add("shake-animation");
-      
+
         icon.addEventListener("animationend", function () {
           icon.classList.remove("shake-animation");
         });
@@ -67,13 +68,14 @@ document.addEventListener("DOMContentLoaded", function () {
         i--;
         likedIteration.textContent = i;
         removeFromLiked(icon);
-        console.log("ID ikony po usunięciu z polubionych:", icon.getAttribute("data-unique-id"));
+        console.log(
+          "ID ikony po usunięciu z polubionych:",
+          icon.getAttribute("data-unique-id")
+        );
 
-        
         if (parentTile) {
-        parentTile.classList.remove("active");
+          parentTile.classList.remove("active");
         }
- 
       }
     });
   });
@@ -81,30 +83,23 @@ document.addEventListener("DOMContentLoaded", function () {
   function addToLiked(icon) {
     if (!likedTiles.includes(icon.getAttribute("data-unique-id"))) {
       likedTiles.push(icon.getAttribute("data-unique-id"));
-
     }
     if (!icon.getAttribute("data-unique-id")) {
-      
-      
       iterator++;
       const uniqueId = "id_" + iterator;
       icon.setAttribute("data-unique-id", uniqueId);
     }
 
-    localStorage.setItem('likedTiles', JSON.stringify(likedTiles));
-    
-    
+    localStorage.setItem("likedTiles", JSON.stringify(likedTiles));
   }
 
   function removeFromLiked(icon) {
     const index = likedTiles.indexOf(icon.getAttribute("data-unique-id"));
     if (index !== -1) {
       likedTiles.splice(index, 1);
-      localStorage.setItem('likedTiles', JSON.stringify(likedTiles));
-      
+      localStorage.setItem("likedTiles", JSON.stringify(likedTiles));
     }
   }
-  
 
   addToLiked(document.querySelector(".fa-heart"));
 
@@ -116,47 +111,40 @@ document.addEventListener("DOMContentLoaded", function () {
       }
       console.log(currentElement);
       currentElement = currentElement.parentElement;
-      
     }
     return null;
   }
-
 });
 
 bookmark.addEventListener("click", function () {
-
- const rmCar = document.querySelector(".carouselItems");
- rmCar.remove();
-const rmDots = document.getElementById("more");
-rmDots.remove();
-const rmWar = document.querySelector(".alert");
-rmWar.remove();
+  const rmCar = document.querySelector(".carouselItems");
+  rmCar.remove();
+  const rmDots = document.getElementById("more");
+  rmDots.remove();
+  const rmWar = document.querySelector(".alert");
+  rmWar.remove();
 
   let likedScreen = document.createElement("div");
   likedScreen.setAttribute("class", "likedScreen");
   document.body.appendChild(likedScreen);
   const myHeader = document.createElement("h4");
   likedScreen.appendChild(myHeader);
-  myHeader.textContent="Polubione miejsca";
-  myHeader.classList.add("myHeader","mt-4");
+  myHeader.textContent = "Polubione miejsca";
+  myHeader.classList.add("myHeader", "mt-4");
 
- preview.forEach(function(showTileActive){
-    if(showTileActive.classList.contains("active")){
+  preview.forEach(function (showTileActive) {
+    if (showTileActive.classList.contains("active")) {
       likedScreen.appendChild(showTileActive);
-      showTileActive.style.display="flex";
+      showTileActive.style.display = "flex";
     }
-    if(!showTileActive.classList.contains("active")){
+    if (!showTileActive.classList.contains("active")) {
       showTileActive.remove();
     }
- });
- bookmark.remove();
+  });
+  bookmark.remove();
 });
 
-
- 
-
-
-dot.addEventListener("click", function() {
+dot.addEventListener("click", function () {
   if (!isShown) {
     console.log("klik!");
     for (let i = 0; i < hiddenSections.length; i++) {
@@ -169,23 +157,82 @@ dot.addEventListener("click", function() {
       for (let i = 0; i < hiddenSections.length; i++) {
         hiddenSections[i].classList.add("active");
       }
-     
     }
   }
 });
 
-eye.addEventListener("click",function(){    
+eye.addEventListener("click", function () {
   eye.classList.add("d-none");
-  
+
   for (let i = 0; i < hiddenSections.length; i++) {
-    hiddenSections[i].classList.add("unactive");  
-    hiddenSections[i].classList.add("d-none"); 
-    hiddenSections[i].classList.remove("active"); 
+    hiddenSections[i].classList.add("unactive");
+    hiddenSections[i].classList.add("d-none");
+    hiddenSections[i].classList.remove("active");
   }
   dot.classList.remove("d-none");
-  isShown=false; 
-
+  isShown = false;
 });
 
+let sendButton = document.querySelectorAll("#sendButton");
+let delay = 1500;
 
+function createSpinner(appendElement) {
+  let spinner = document.createElement("span");
+  spinner.classList.add("spinner-border");
+  spinner.classList.add("spinner-border-sm");
+  spinner.setAttribute("aria-hidden", "true");
+  spinner.setAttribute("id", "spinner");
+  appendElement.appendChild(spinner);
+}
+let emailValue = document.getElementById("email_id").value;
+let messageValue = document.getElementById("message").value;
+let dateValue = document.getElementById("date").value;
+let fromTimeValue = document.getElementById("startTime").value;
+let toTimeValue = document.getElementById("endTime").value;
 
+function SendMail(placeName) {
+  if (!emailValue || !dateValue) {
+    console.log("Wprowadź adres e-mail i wiadomość przed wysłaniem.");
+
+    return;
+  }
+
+  let params = {
+    email_id: emailValue,
+    message: messageValue,
+    date: dateValue,
+    place: placeName,
+    since: fromTimeValue,
+    to: toTimeValue,
+  };
+
+  emailjs
+    .send("service_ul8bste", "template_3w483ke", params)
+    .then(function (res) {
+      console.log("Success! " + res.status);
+    });
+}
+
+let lowDelay = 1000;
+sendButton.forEach(function (e) {
+  e.addEventListener("click", function () {
+    console.log("klik");
+    let placeName = e.getAttribute("data-place-name");
+
+    SendMail(placeName);
+    console.log("Email:" + emailValue);
+
+    e.textContent = "";
+    createSpinner(e);
+
+    // Po opóźnieniu przywróć klasę "d-none" dla elementu spinnera i tekstu ładowania
+    setTimeout(function () {
+      createSpinner(e);
+
+      e.textContent = "Sukces!";
+      setTimeout(function () {
+        e.textContent = "Wyślij!";
+      }, lowDelay);
+    }, delay);
+  });
+});
